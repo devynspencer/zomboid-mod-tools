devynspencer/zomboid-mod-tools

commit 56995bb7a0247cdd92c07d58d92d81c9968d9e79

Refactor Find-ZomboidMod
Separate logic into separate function(s) as much as possible. Doing so while also adding a new public function is a nice bonus.

ZomboidModTools/public/Find-ZomboidMod.ps1
@@ -1,3 +1,4 @@
+. "$PSScriptRoot\Get-ZomboidModLocation.ps1"
 . "$PSScriptRoot\Get-ZomboidModInfo.ps1"
 
 function Find-ZomboidMod {
@@ -11,35 +12,11 @@ function Find-ZomboidMod {
         # path structure, as they are stored under a Steam Workshop id.
         [ValidateSet('User', 'Local', 'Workshop')]
         $Location = 'User',
-
-        # Path to the Zomboid mods directory within the user profile
-        [ValidateScript({ Test-Path $_ })]
-        $UserModsRoot = "$env:USERPROFILE\Zomboid\mods",
-
-        # Path to the Zomboid mods directory within the install directory
-        [ValidateScript({ Test-Path $_ })]
-        $LocalModsRoot = 'C:\Program Files (x86)\Steam\steamapps\common\ProjectZomboid\mods',
-
-        # Path to Zomboid mods directory for mods installed via Steam Workshop
-        [ValidateScript({ Test-Path $_ })]
-        $WorkshopModsRoot = 'C:\Program Files (x86)\Steam\steamapps\workshop\content\108600'
     )
 
     Write-Verbose "Looking for mods containing name: [$ModName]"
 
-    switch ($Location) {
-        'User' {
-            $ModRootPath = $UserModsRoot
-        }
-
-        'Local' {
-            $ModRootPath = $LocalModsRoot
-        }
-
-        'Workshop' {
-            $ModRootPath = $WorkshopModsRoot
-        }
-    }
+    $ModRootPath = (Get-ZomboidModLocation -Location $Location).Path
 
     $ModInfoFiles = Get-ChildItem -Path $ModRootPath -Recurse -Depth 3 -Filter 'mod.info'
 

commit c051c57462039df7136fb67c54bf04073e932c5a

Fix Find-Zomboid function
Fixes issue introduced in 56995bb7a0247cdd92c07d58d92d81c9968d9e79

ZomboidModTools/public/Find-ZomboidMod.ps1
@@ -10,7 +10,7 @@ function Find-ZomboidMod {
         # Project Zomboid mod directory to resolve. Workshop mods have a different
         # path structure, as they are stored under a Steam Workshop id.
         [ValidateSet('User', 'Local', 'Workshop')]
-        $Location = 'User',
+        $Location = 'User'
     )
 
     $ChildParams = @{