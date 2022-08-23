5fab024a62c6e1580ec5b3160b70f6099754e40b
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-08-22 18:07:56 -0700

    Add Get-ZomboidModLocation function

--- ZomboidModTools/ZomboidModTools.psd1
+++ ZomboidModTools/ZomboidModTools.psd1
@@ -73,6 +73,7 @@
         'Start-Zomboid'
         'Show-ZomboidLog'
         'Install-SteamCmd'
+        'Get-ZomboidModLocation'
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
---
+++ ZomboidModTools/public/Get-ZomboidModLocation.ps1
@@ -0,0 +1,24 @@
+function Get-ZomboidModLocation {
+    [CmdletBinding()]
+    param (
+        # Named location(s) where Project Zomboid loads mods from
+        [ValidateSet('User', 'Local', 'Workshop')]
+        $Location = @('User', 'Local', 'Workshop')
+    )
+
+    $BaseLocations = @{
+        User = "$env:USERPROFILE\Zomboid\mods"
+        Local = 'C:\Program Files (x86)\Steam\steamapps\common\ProjectZomboid\mods'
+        Workshop = 'C:\Program Files (x86)\Steam\steamapps\workshop\content\108600'
+    }
+
+    foreach ($LocationName in $Location) {
+        $Path = $BaseLocations[$LocationName]
+
+        # Build output object
+        [pscustomobject] @{
+            Name = $LocationName
+            Path = $Path
+        }
+    }
+}
