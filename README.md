f2f2b84ff783f0bcc1439e7abf6b8ee01b148438
Devyn Spencer <devynspencer@users.noreply.github.com>
2023-12-08 12:20:42 -0800

    Copy configuration with Update-ZomboidSourceMap

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Project Zomboid Development Tools
 
 Development tools, configurations, templates, and build scripts for Project Zomboid mod development.
+
+## Features
+
+### EmmyLua
+
+The module deploys a collection of [EmmyLua](https://emmylua.github.io/) typings for the Project Zomboid source code. See the [project page](https://github.com/asledgehammer/Umbrella) on GitHub for more information on how the typings are generated.
+
+This project is also designed to support development in Visual Studio Code using the [EmmyLua extension](https://github.com/EmmyLua/VSCode-EmmyLua). A basic configuration file for the extension is copied to the project directory by default:
+
+```powershell
+Update-ZomboidSourceMap -Destination "$env:USERPROFILE\Zomboid\mods\ModName"
+```
+
+To skip this step, use the `-SkipConfig` switch.
--- ZomboidModTools/public/Update-ZomboidSourceMap.ps1
+++ ZomboidModTools/public/Update-ZomboidSourceMap.ps1
@@ -4,6 +4,13 @@ function Update-ZomboidSourceMap {
         [ValidateScript({ Test-Path -LiteralPath $_ })]
         $Destination,
 
+        [switch]
+        $SkipConfig,
+
+        # Path to config file template for VSCode EmmyLua extension
+        [ValidateScript({ Test-Path -LiteralPath $_ })]
+        $ConfigFilePath = "$PSScriptRoot\..\templates\emmy.config.json",
+
         # The URI of the repository to clone
         $Uri = 'https://github.com/asledgehammer/Umbrella'
     )
@@ -21,4 +28,17 @@ function Update-ZomboidSourceMap {
     # Update submodules
     $SourceMapRoot = Join-Path -Path $Destination -ChildPath 'emmy'
     Start-Process @GitParams -WorkingDirectory $SourceMapRoot -ArgumentList 'submodule', 'update', '--init', '--remote'
+
+    if (!$SkipConfig) {
+        Write-Verbose "Copying EmmyLua configuration from template to $SourceMapRoot"
+
+        # Copy EmmyLua configuration from template
+        $CopyParams = @{
+            Path = $ConfigFilePath
+            Destination = $SourceMapRoot
+            Force = $true
+        }
+
+        Copy-Item @CopyParams
+    }
 }
